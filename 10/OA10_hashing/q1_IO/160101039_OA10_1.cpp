9d22278801527ef2cb69c27fc5e7fced8abc4eb8
kapil-goyal <kapil.goyal61@gmail.com>
2017-11-05 01:47:53 +0530

    optimised not working

--- 10/OA10_hashing/q1_IO/160101039_OA10_1.cpp
+++ 10/OA10_hashing/q1_IO/160101039_OA10_1.cpp
@@ -11,19 +11,36 @@ int hash_val(string str, int m, int n, int base, int prime) {
 	return result;
 }
 
+int hash_val_substr(int *hash_val_arr, int i, int j, int base, int prime) {
+
+	i = i-1;
+	if(i < 0)
+		return hash_val_arr[j];
+	int x = hash_val_arr[i],y = hash_val_arr[j];
+	for (int _ = 0; _ < j-i; ++_) {
+		x = (base*x);//%prime;
+	}
+	return (y-x%prime)%prime;
+}
+
 int main()
 {
 	string str;
-	int N, base=7, prime=101;
+	int N, base=26, prime=23;
 	cin >> str >> N;
+	int len = str.length();
+	int *hash_val_arr = new int [len];
+	for (int i = 0; i < len; ++i) {
+		hash_val_arr[i] = hash_val(str,1,i+1,base,prime);
+	}
 	int ind_1s, ind_1e;
 	int ind_2s, ind_2e;
 
 	for (int _ = 0; _ < N; ++_)
 	{
 		cin >> ind_1s >> ind_1e >> ind_2s >> ind_2e;
-		int hash_value1 = hash_val(str,ind_1s,ind_1e,base,prime);
-		int hash_value2 = hash_val(str,ind_2s,ind_2e,base,prime);
+		int hash_value1 = hash_val_substr(hash_val_arr,ind_1s-1,ind_1e-1,base,prime);
+		int hash_value2 = hash_val_substr(hash_val_arr,ind_2s-1,ind_2e-1,base,prime);
 
 		if(hash_value1 != hash_value2)
 			cout << "NO\n";
